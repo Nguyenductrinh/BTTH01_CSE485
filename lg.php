953bbce0e24fae8ccd30e4480bffe0d07465b288
Nguyenductrinh <86778595+Nguyenductrinh@users.noreply.github.com>
2023-02-28 11:08:52 +0700

    add file lg.php

--- lg.php
+++ lg.php
@@ -1,25 +1,13 @@
 <?php 
     require'connect.php';
-    if(isset($_POST['btn-lg'])){
-        $username = $_POST['ten_dnhap'];
-        $passwword = $_POST['mat_khau'];
-
-        if(!empty($username) && !empty($password)){
-            //insert dữ liệu
-            // echo"<pre>";
-            // print_r($_POST);
-
-            $sql = "SELECT * FROM user WHERE ten_dnhap= '".$username."' and mat_khau='".$passwword."'";
-
-            if($conn->query($sql)===TRUE){
-                header('localhost:admin/index.php');
-            }
-            else{
-                echo"Lỗi {$sql}".$conn->errno;
-            }
-        }
-        else{
-            echo"Tài khoản hoặc mật khẩu sai.Vui lòng nhập lại!";
+    if($_POST){
+        $user_name = $_POST['ten_dnhap'];
+        $user_pass = $_POST['mat_khau'];
+        $sql = "SELECT * FROM user WHERE ten_dnhap='$user_name' AND mat_khau ='$user_pass';";
+        $result=mysqli_query($conn,$sql);
+        $row = mysqli_fetch_assoc($result);
+        if($row) {
+            header('Location:./admin/index.php');
         }
     }
 ?>
